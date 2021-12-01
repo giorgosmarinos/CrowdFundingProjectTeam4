9aa985f19cbc85153689f8a6ef9897d2ed5e1f39
giorgos marinos <geo_marinos@outlook.com.gr>
2021-12-01 19:58:03 +0200

    initial (test) service and interface

---
+++ CrowdFundingProjectTeam4/Service/IUserActionsService.cs
@@ -0,0 +1,14 @@
+﻿using CrowdFundingProjectTeam4.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrowdFundingProjectTeam4.Service
+{
+    public interface IUserActionsService
+    {
+        public User GetUser(int id);
+    }
+}
---
+++ CrowdFundingProjectTeam4/Service/UserActionsService.cs
@@ -0,0 +1,42 @@
+﻿using CrowdFundingProjectTeam4.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrowdFundingProjectTeam4.Service
+{
+    public class UserActionsService: IUserActionsService
+    {
+        private readonly CrowdFundingTeam4DBContext dbContext;
+
+        public UserActionsService(CrowdFundingTeam4DBContext adbContext)
+        {
+            adbContext = dbContext;
+        }
+
+        User IUserActionsService.GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int id)
+        {
+            User user = dbContext.User.Find(id);
+            return user;
+            //try { dbContext.SaveChanges(); }  should we save changes here?
+            //catch { }
+        }
+
+        //this one may be not necessary, there is not is the diagram
+        /*
+        public List<User> ReadUsers()
+        {
+            return dbContext.User.ToList();
+        }
+        */
+
+
+    }
+}
