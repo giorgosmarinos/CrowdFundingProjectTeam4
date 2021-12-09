3717bb4787d7dd3765103db1e141f9a75859003a
George Bekos <george-bekos@outlook.com>
2021-12-09 06:30:44 +0200

    Controller

--- FundableMvc/Models/Funding.cs
+++ FundableMvc/Models/Funding.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,12 @@ namespace FundableMvc.Models
     {
         public List<Funding> Project { get; set; }
         public List<int> ProjectIds { get; set; }
+        
+
+        public Funding(SelectList funding)
+        {
+            Funding = funding;
+        }
     }
 
     public class FundingPackage
@@ -16,4 +23,5 @@ namespace FundableMvc.Models
         public int Funding { get; set; }
         public string Text { get; set; }
     }
+
 }
